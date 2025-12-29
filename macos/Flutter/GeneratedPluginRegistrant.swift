0ccdc05f9ea98d0ab918400e05af13e373d13b5e
x <5vnetwork@proton.me>
2025-12-30 03:37:56 +0800

    rpm change

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -31,7 +31,6 @@ import sign_in_with_apple
 import sqflite_darwin
 import sqlite3_flutter_libs
 import tm_macos
-import tray_manager
 import url_launcher_macos
 import window_manager
 
@@ -62,7 +61,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   Sqlite3FlutterLibsPlugin.register(with: registry.registrar(forPlugin: "Sqlite3FlutterLibsPlugin"))
   TmMacosPlugin.register(with: registry.registrar(forPlugin: "TmMacosPlugin"))
-  TrayManagerPlugin.register(with: registry.registrar(forPlugin: "TrayManagerPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
   WindowManagerPlugin.register(with: registry.registrar(forPlugin: "WindowManagerPlugin"))
 }
