49aa14a131f6ebfe51e8268f1551860d8920d9c5
x <5vnetwork@proton.me>
2026-09-22 01:12:27 +0800

    android nicmon

--- android/app/src/main/kotlin/com5vnetwork/vproxy/AndroidHostApi.kt
+++ android/app/src/main/kotlin/com5vnetwork/vproxy/AndroidHostApi.kt
@@ -26,7 +26,7 @@ class AndroidHostApiImpl(private val context: Context) : AndroidHostApi {
         } catch (e: Exception) {
             callback(Result.failure(FlutterError("", e.toString(), "")))
         }
-//        startMonitorDefaultNIC()
+        startMonitorDefaultNIC()
         callback(Result.success(Unit))
     }
 
@@ -62,72 +62,72 @@ class AndroidHostApiImpl(private val context: Context) : AndroidHostApi {
         }
     }
 
-//    private val mainHandler = Handler(Looper.getMainLooper())
-//    private fun startMonitorDefaultNIC() {
-//        val connectivityManager = context.getSystemService(ConnectivityManager::class.java)
-//        // start monitor dns servers of the default nic
-//        val networkRequest = NetworkRequest.Builder()
-//            .addCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
-//            .removeTransportType(NetworkCapabilities.TRANSPORT_VPN)
-//            .build()
-//        networkCallback = object : ConnectivityManager.NetworkCallback() {
-//            override fun onAvailable(network: Network) {
-//                super.onAvailable(network)
-//                Log.d("NetworkChangeMonitor", "Network is available $network")
-////                networkCallback(network)
-//            }
-//
-//            override fun onLinkPropertiesChanged(network: Network, linkProperties: LinkProperties) {
-//                super.onLinkPropertiesChanged(network, linkProperties)
-//                Log.d("NetworkChangeMonitor", "Link properties changed: $linkProperties")
+    private val mainHandler = Handler(Looper.getMainLooper())
+    private fun startMonitorDefaultNIC() {
+        val connectivityManager = context.getSystemService(ConnectivityManager::class.java)
+        // start monitor dns servers of the default nic
+        val networkRequest = NetworkRequest.Builder()
+            .addCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+            .removeTransportType(NetworkCapabilities.TRANSPORT_VPN)
+            .build()
+        networkCallback = object : ConnectivityManager.NetworkCallback() {
+            override fun onAvailable(network: Network) {
+                super.onAvailable(network)
+                Log.d("NetworkChangeMonitor", "Network is available $network")
 //                networkCallback(network)
-//            }
-//        }
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-//            connectivityManager.registerBestMatchingNetworkCallback(networkRequest, networkCallback!!, mainHandler)
-//        } else {
-//            networkCallback = null
-//        }
-//    }
-//
-//    // get current active network and notify nic listeners
-//    private fun networkCallback(network: Network) {
-//        val connectivityManager = context.getSystemService(ConnectivityManager::class.java)
-////        val active = connectivityManager.activeNetwork
-//        val linkProperties = connectivityManager.getLinkProperties(network)
-//        if (linkProperties != null) {
-//            val dnsServers = ArrayList<String?>()
-//            for (inetAddress in linkProperties.dnsServers) {
-//                dnsServers.add(inetAddress.hostAddress)
-//            }
-//            val dnsServerList = object : StringList {
-//                val l = linkProperties.dnsServers.size.toLong()
-//                val strings = dnsServers
-//                override fun get(var1: Long): String? {
-//                    return strings[var1.toInt()]
-//                }
-//
-//                override fun len(): Long {
-//                    return l
-//                }
-//            }
-//            val linkAddresses = ArrayList<String?>()
-//            for (linkAddress in linkProperties.linkAddresses) {
-//                linkAddresses.add(linkAddress.address.hostAddress)
-//            }
-//            val nicAddressList = object : StringList {
-//                val l = linkProperties.linkAddresses.size.toLong()
-//                val strings = linkAddresses
-//                override fun get(var1: Long): String? {
-//                    return strings[var1.toInt()]
-//                }
-//
-//                override fun len(): Long {
-//                    return l
-//                }
-//            }
-//            X_android.updateDefaultNICInfo(linkProperties.interfaceName, nicAddressList, dnsServerList)
-//        }
-//    }
+            }
+
+            override fun onLinkPropertiesChanged(network: Network, linkProperties: LinkProperties) {
+                super.onLinkPropertiesChanged(network, linkProperties)
+                Log.d("NetworkChangeMonitor", "Link properties changed: $linkProperties")
+                networkCallback(network)
+            }
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            connectivityManager.registerBestMatchingNetworkCallback(networkRequest, networkCallback!!, mainHandler)
+        } else {
+            networkCallback = null
+        }
+    }
+
+    // get current active network and notify nic listeners
+    private fun networkCallback(network: Network) {
+        val connectivityManager = context.getSystemService(ConnectivityManager::class.java)
+//        val active = connectivityManager.activeNetwork
+        val linkProperties = connectivityManager.getLinkProperties(network)
+        if (linkProperties != null) {
+            val dnsServers = ArrayList<String?>()
+            for (inetAddress in linkProperties.dnsServers) {
+                dnsServers.add(inetAddress.hostAddress)
+            }
+            val dnsServerList = object : StringList {
+                val l = linkProperties.dnsServers.size.toLong()
+                val strings = dnsServers
+                override fun get(var1: Long): String? {
+                    return strings[var1.toInt()]
+                }
+
+                override fun len(): Long {
+                    return l
+                }
+            }
+            val linkAddresses = ArrayList<String?>()
+            for (linkAddress in linkProperties.linkAddresses) {
+                linkAddresses.add(linkAddress.address.hostAddress)
+            }
+            val nicAddressList = object : StringList {
+                val l = linkProperties.linkAddresses.size.toLong()
+                val strings = linkAddresses
+                override fun get(var1: Long): String? {
+                    return strings[var1.toInt()]
+                }
+
+                override fun len(): Long {
+                    return l
+                }
+            }
+            X_android.updateDefaultNICInfo(linkProperties.interfaceName, nicAddressList, dnsServerList)
+        }
+    }
 
 }
